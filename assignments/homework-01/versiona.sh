00a92ab79c054c9463332f7fcdc1c99752c554ec
Scott Schumacher <scottmachershoe@yahoo.com>
2016-09-12 10:20:31 -0400

    edited versiona.sh and added versionb.sh

--- assignments/homework-01/versiona.sh
+++ assignments/homework-01/versiona.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 now=$(date +"%Y-%m-%d")
-echo $now
 fname=$1
-echo $fname
 blurp=_
-newname=$now$blurp$fname
-echo $newname
+echo $now$blurp$fname
---
+++ assignments/homework-01/versionb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+now=$(date +%Y-%m-%d)
+fname=$1
+blurp=_
+echo $now$blurp$fname
